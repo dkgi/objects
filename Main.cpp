fb5d4c0145a5b7830295ee1e552db80e3abb22e6
Dominik Gabi <dkgispam@gmail.com>
2012-04-03 22:34:28 +0200

    Basic camera controls implemented

--- Main.cpp
+++ Main.cpp
@@ -2,34 +2,45 @@
 #include <GL/glu.h>
 #include <GL/freeglut.h>
 #include <iostream>
+#include <iomanip>
 #include <vector>
+#include <cassert>
 
 #include "Context.h"
+#include "Vector.h"
 
 using namespace Objects;
 
-InputContext ic;
-SimulationContext sc;
+InputContext input;
+SimulationContext simulation;
 
-std::vector<float> m_position(3, 0.0); 
+// TODO move into context
+// TODO up should be dynamic
+Vector camera(0.0, 0.0, -5.0); 
+Vector up(0.0, 1.0, 0.0);
+float speed = 5.0;
 
 void init()
 {
-	glutWarpPointer(ic.mouse[0], ic.mouse[1]);
-m_position[2] = -5.0;
+	std::cout << std::fixed << std::setprecision(2);
+	glutWarpPointer(input.mouse.defaultPosition(0), input.mouse.defaultPosition(1));
 }
 
 void idle()
 {
-	sc.now = glutGet(GLUT_ELAPSED_TIME);
-	sc.dt = (sc.now - sc.then) / 1000.0f;
-	sc.then = sc.now;
+	simulation.now = glutGet(GLUT_ELAPSED_TIME);
+	simulation.dt = (simulation.now - simulation.then) / 1000.0f;
+	simulation.then = simulation.now;
 
 	// Move camera
-	if (ic.keyDown['s']) m_position[2] -= 0.1;
-	if (ic.keyDown['w']) m_position[2] += 0.1;
+	Vector dz = input.mouse.orientation * speed * simulation.dt;
+	Vector dx = input.mouse.orientation.cross(up) * speed * simulation.dt;
+	if (input.keyDown['s']) camera -= dz;
+	if (input.keyDown['w']) camera += dz;
+	if (input.keyDown['a']) camera -= dx;
+	if (input.keyDown['d']) camera += dx;
 
-	// Move scene
+	// Move simulation
 
 	// Redisplay
 	glutPostRedisplay();
@@ -42,9 +53,12 @@ void display()
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	// Camera transformation
-	glTranslatef(m_position[0], m_position[1], m_position[2]);
+	Vector at = camera + input.mouse.orientation;
+	gluLookAt(camera(0), camera(1), camera(2), 
+			at(0), at(1), at(2),
+			up(0), up(1), up(2));
 
-	// Render scene
+	// Render scene 
 	glutWireTeapot(1.0);
 
 	glutSwapBuffers();
@@ -52,32 +66,39 @@ void display()
 
 void reshape(int w, int h)
 {
+	input.mouse.adjustReference(w, h);
 	glViewport(0, 0, w, h);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	gluPerspective(45.0, (float)w/h, 0.1, 500);
+	glutWarpPointer(input.mouse.defaultPosition(0), input.mouse.defaultPosition(1));
 }
 
 void keyboard(unsigned char key, int x, int y)
 {
-	ic.keyDown[key] = true;
+	input.keyboard(key, x, y);
 
 	switch(key) {
 		case 'q':
 			exit(0);
+			break;
+		case 'f':
+			glutFullScreenToggle();
 			break;
 	}
 }
 
 void keyboardUp(unsigned char key, int x, int y)
 {
-	ic.keyDown[key] = false;
+	input.keyboardUp(key, x, y);
 }
 
 void passiveMotion(int x, int y)
 {
-	glutWarpPointer(ic.mouse[0], ic.mouse[1]);
+	input.mouse.passiveMotion(x, y);
+	glutWarpPointer(input.mouse.defaultPosition(0), input.mouse.defaultPosition(1));
 }
+
 
 int main(int argc, char *argv[])
 {
